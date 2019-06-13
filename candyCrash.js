c3e785364da703f2679a405af99e4783560242a6
lonelyarcher <xumeng@gmail.com>
2019-06-13 17:58:08 -0400

    candyCrash

---
+++ candyCrash.js
@@ -0,0 +1,15 @@
+const candyCrash = (s) => {
+    const st = [];
+    for (let i = 0; i < s.length; i++) {
+        const c = s.charAt(i);
+        if (st.length > 1 && c === st[0] && c === st[1]) {
+            st.shift();
+            st.shift();
+        } else {
+            st.unshift(c);
+        }
+    }
+    return st.reduce((a, b) => b + a);
+}
+
+console.info(candyCrash("aabccdddcbbaeeefffgh"));
