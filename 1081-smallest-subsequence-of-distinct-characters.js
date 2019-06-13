848ab1dffe2057b09ab2869b63d7b133b56ff418
lonelyarcher <xumeng@gmail.com>
2019-06-12 22:04:05 -0400

    1081

--- 1081-smallest-subsequence-of-distinct-characters.js
+++ 1081-smallest-subsequence-of-distinct-characters.js
@@ -3,5 +3,19 @@
  * @return {string}
  */
 var smallestSubsequence = function(text) {
-    
+    const st = [], picked = {};
+    let ans = '';
+    const last = [...text].reduce((a, b, i) => Object.assign(a, {[b.charCodeAt(0) - 'a'.charCodeAt(0)]:  i}), {});
+    for( let i = 0; i < text.length; i++) {
+        const c = text.charCodeAt(i) - 'a'.charCodeAt(0);
+        if (picked[c]) continue;
+        while (st.length && c <= st[0] && i < last[st[0]]) {
+            picked[st.shift()] = false;
+        }
+        st.unshift(c);
+        picked[c] = true;
+    }
+    return st.reduce((a, b) => a = String.fromCharCode(b + 'a'.charCodeAt(0)) + a, '');
 };
+console.info("test:");
+console.info(smallestSubsequence("cdadabcc")); //adbc
