a18096842c6d375c0d69e1cf45bf64da7864f876
lonelyarcher <xumeng@gmail.com>
2019-06-22 21:27:05 -0400

    wip

---
+++ ObjecClassFunction.js
@@ -0,0 +1,26 @@
+const obj = { a: 1, b: "hello" };
+const objB = Object.create(obj);
+console.info(objB.b);
+
+const Node = function(val){  //counstructor function can't be arrow function, because arrow funciton doesn't has it own "this"
+    this.val = val;
+    this.left = null;
+    this.right = null;
+};
+
+const root = new Node(1);
+console.info(root.val);
+
+
+// Example using a arrow function
+function createObject() {  //inside the function, "this" is decided by who calls this function {foo: 21}
+    console.log('Inside `createObject`:', this.foo);
+    return {
+      foo: 42,
+      bar: () => console.log('Inside `bar`:', this.foo), 
+      //inside arrow function, "this" is decided by lexical ( means "this" in createObject function, which is same as the who call createObject {foo: 21}
+      //if it is a function, bar: function() { console.log('Inside `bar`:', this.foo)} then it is who call bar(), which is new Object {foo: 42}
+    };
+  }
+  
+createObject.call({foo: 21}).bar(); // override `this` inside createObject
---
+++ largestValuesFromLabels.js
@@ -0,0 +1,10 @@
+var largestValsFromLabels = function(values, labels, num_wanted, use_limit) {
+    const cmp = (a, b) => a - b;    
+    const map = {}
+    for (let i = 0; i < values.length; i++) {
+        map[labels[i]] = (map[labels[i]] || []).concat([values[i]]);
+    }
+    return Object.values(map).map(list => list.sort(cmp).slice(-use_limit)).reduce((a, c) => a.concat(c)).sort(cmp).slice(-num_wanted).reduce((a, c) => a + c);
+};
+console.info(Array.prototype.flat);
+console.info(largestValsFromLabels(values = [5,4,3,2,1], labels = [1,1,2,2,3], num_wanted = 3, use_limit = 1));
