c88675335fd40dabf2496f3a810f7fc0c11620cf
meng <xumeng@gmail.com>
2019-07-14 23:50:51 -0400

    Create implementAsyncAwait.js

---
+++ implementAsyncAwait.js
@@ -0,0 +1,42 @@
+const api1 = new Promise((resolve, reject) => {
+  const data = {name: 'John'}
+  setTimeout(() => {resolve(data);}, 1000);
+});
+
+function *generator() {
+  try {  //try catch could not catch async error, but promise then() err to make the gen throw erro and catch here.
+    const response = yield api1; //async call api1
+    console.log(`Response: ${response}`); //await to get response. Without yield, you will get undefined. 
+  } catch (err) {
+    console.log(`Err: ${err}`);
+  }
+}
+
+const iter = generator();
+
+iter.next().value.then(
+  res => iter.next(res.name), // pass the response data to next
+  err => iter.throw(`Err: ${err}`) //throw the error in gen 
+);
+
+
+run(generator) {
+  const iter = generator();
+  const handleNext = (value) => {
+    const next = iter.next(value);
+    if (next.done) {
+      return next.value;
+    } else {
+      return Promise.resolve(next.value).then(
+        handleNext,
+        err => Promise.resolve(iter.throw(`Err: ${err}`)).then(handleNext); //throw the error in gen 
+      );
+    }
+  };
+  iter.next().value.then(
+    res => iter.next(res.name), // pass the response data to next
+    err => iter.throw(`Err: ${err}`) //throw the error in gen 
+  );
+}
+
+
