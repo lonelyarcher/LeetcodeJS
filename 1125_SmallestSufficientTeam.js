1fa47d68dbff967a7d881bd2480007e8798ee2da
lonelyarcher <xumeng@gmail.com>
2019-07-17 23:23:31 -0400

    wip

--- 1125_SmallestSufficientTeam.js
+++ 1125_SmallestSufficientTeam.js
@@ -22,31 +22,51 @@ It is guaranteed a sufficient team exists. */
  */
 //dfs
 var smallestSufficientTeam = function(req_skills, people) {
-    
+    const map = req_skills.reduce((a, c, i) => { a[c] = 1 << i; return a; }, {});
+    const req = (1 << req_skills.length) - 1; // '<<' precedence after '+-', &^| more lower, but !~ higher than */ 
+    const pskills = people.map(person => person.reduce((a, c) => a|map[c], 0));
+    let sft = people.map((p, i) => i);
+    const dfs = (cur, team) => { //dfs need path/cur (may be mutilple),  to record search result, once it satified, then make copy of it to answer.
+        //sometime needs visited to record visited elements, which will be backtracked later.
+        if (team.length >= sft.length) return;
+        if (cur === req) {
+            sft = team.slice();  //DFS with backtracking, when result come out, must make copy otherwise it will changed after backtracking
+            return;
+        }
+        pskills.forEach((p, i) => {
+            if ((cur | p) > cur) {
+                team.push(i);
+                dfs(cur|p, team);
+                team.pop();  //backtracking
+            }
+        }); 
+    };
+    dfs(0, []);
+    return sft;
 };
 
 //bfs
-var smallestSufficientTeam = function(req_skills, people) {
+// var smallestSufficientTeam = function(req_skills, people) {
     
-};
+// };
 
-//dp
-var smallestSufficientTeam = function(req_skills, people) {
-    const req = 1 << req_skills.length;
-    const map = req_skills.reduce((a, c, i) => { a[c] = 1 << i; return a; }, {});
-    const p = people.map(person => person.reduce((a, c) => a|map[c], 0));
-    const dp = [...Array(req)].map(r => Array(p.length + 1).fill(undefined));
-    dp[0] = Array(p.length + 1).fill([]); 
-    for (let i = 1; i < req; i++) {
-        for (let j = 1; j <= people.length; j++) {
-            const noPick = dp[i][j - 1] ? dp[i][j - 1].length : Infinity;
-            const ni = i ^ (i & p[j - 1]);
-            const pick = dp[ni][j - 1] ? dp[ni][j - 1].length + 1: Infinity;
-            dp[i][j] = noPick <= pick ? dp[i][j - 1] : dp[ni][j - 1].concat([j - 1]);
-        }
-    }
-    return dp[req - 1][p.length];
-};
+//dp with status compression (by bit operation to an integer)
+// var smallestSufficientTeam = function(req_skills, people) {
+//     const req = 1 << req_skills.length;
+//     const map = req_skills.reduce((a, c, i) => { a[c] = 1 << i; return a; }, {});
+//     const p = people.map(person => person.reduce((a, c) => a|map[c], 0));
+//     const dp = [...Array(req)].map(r => Array(p.length + 1).fill(undefined));
+//     dp[0] = Array(p.length + 1).fill([]); 
+//     for (let i = 1; i < req; i++) {
+//         for (let j = 1; j <= people.length; j++) {
+//             const noPick = dp[i][j - 1] ? dp[i][j - 1].length : Infinity;
+//             const ni = i ^ (i & p[j - 1]);
+//             const pick = dp[ni][j - 1] ? dp[ni][j - 1].length + 1: Infinity;
+//             dp[i][j] = noPick <= pick ? dp[i][j - 1] : dp[ni][j - 1].concat([j - 1]);
+//         }
+//     }
+//     return dp[req - 1][p.length];
+// };
 
 
 
@@ -60,4 +80,3 @@ console.log(smallestSufficientTeam(req_skills, people));
 req_skills = ["algorithms","math","java","reactjs","csharp","aws"], people = [["algorithms","math","java"],["algorithms","math","reactjs"],["java","csharp","aws"],["reactjs","csharp"],["csharp","math"],["aws","java"]];
 console.log(smallestSufficientTeam(req_skills, people));
 //Output: [1,2]
- 
---
+++ bitOperator.js
@@ -0,0 +1,4 @@
+console.log(1|undefined); //undefined with bit operation will be 0
+console.log(1 + undefined);
+
+//precedence:  () [].new ++-- !~typeof */%  +- >><< <> instanceof === & ^ | && || = yield , 
