928ce240cc7980047704b240c612ce327904949e
meng <xumeng@gmail.com>
2019-07-25 22:49:43 -0400

    Update TimeComplexAndInputRange.js

--- TimeComplexAndInputRange.js
+++ TimeComplexAndInputRange.js
@@ -1,7 +1,8 @@
 Range: 
-1 - 16, imply bit opertion on an integer  Byte = 8 bit
-< 50,  exponential
-< 100, n^3
-<1000, n^2
-<10^6, n || nlogn
-2^32, lgn 
+1 - 16, imply bit opertion on an integer  Byte = 8 bit, O(n!) permutation, O(2^n) combination, search (brute force dfs)
+< 50,  O(n^4)  dp
+< 100, O(n^3)  dp all pair distince
+<1000, O(n^2)  dp
+<10^6, O(nlogn) sort base greedy, heap, divid & conquer 
+<10^7, O(n) dp, tree traversal, topological sort(V+E), graphic traversal
+2^32,  O(lgn) (binary search), O(sqrt(n)) is prime number? square sum, O(1) math 
