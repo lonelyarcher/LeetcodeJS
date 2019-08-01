f5989a2cd040fd7dc59530fe2d48a8371fcd1fa7
xumeng@gmail.com <xumeng@gmail.com>
2019-08-01 16:15:07 -0400

    wip

--- _MinimumAdjustmentCost.js
+++ _MinimumAdjustmentCost.js
@@ -5,3 +5,25 @@ You can assume each number in the array is a positive integer and not greater th
 Example
 Given [1,4,2,3] and target = 1, one of the solutions is [2,3,2,3], the adjustment cost is 2 and it's minimal.
 Return 2. */
+
+const miniAdjustmentCost = (A, a) => {
+    const n = A.length;
+    const dp = Array(n).fill(0).map(() => Array(100).fill(Infinity));
+    const minAdj = (i, t) => {
+        const diff = Math.abs(A[i] - t) - a;
+        return diff < 0 ? 0 : diff;
+    };
+    dp[0] = Array(100).fill(0).map((c, i) => minAdj(0, i));
+    console.log(dp[0].join());
+    for (let i = 1; i < n; i++) {
+        for (let j = 1; j < 100; j++) {
+            for (let k = Math.max(j - a, 1); k < Math.min(j + a, 99); k++) {
+                dp[i][j] = Math.min(dp[i - 1][k] + minAdj(i, j), dp[i][j]);
+            }
+        }
+    }
+    console.log(dp[n - 1].join());
+    return dp[n - 1][A[n - 1]];
+};
+
+console.log(miniAdjustmentCost([1,4,2,3], 1));
