5715f9e9f83111bf40ec8cca3f524a47ecfb93b2
lonelyarcher <xumeng@gmail.com>
2019-08-09 23:15:41 -0400

    wip

--- _1036_EscapeaLargeMaze.js
+++ _1036_EscapeaLargeMaze.js
@@ -20,47 +20,68 @@ source != target */
  * @return {boolean}
  */
 /* 
-bfs or dfs, dfs is better, because this question is about how source could escape out of the maxium range of block.
-First get maxium range of blocks, [minX, minY][maxX, maxY] 
-1. both source and target are in range. do dfs in the range. 
-2. both are not in the range, return true;
-3. one of them is in the range, try dfs to escape out range , if success, then return true;
-blocks convert into a Map use numeric as key to save space. 
+bfs or dfs, dfs is better, because this question is about how source could escape out of the range of block wall.
+Total block is 200, so at most if deltaX: (new_x - original_x) + deltaY > 200, it must escape out of the walls.
+
+
+1. both source and target are in the wall, so both can not be out but they can meet in well. return true
+2. both can escape out, return true;
+3. one in the wall, one out, they can't meet, return false 
+
+Performance: Map with numeric keys is much faster than object with string key.
+
 */
-const hash = c => c[0]*1000000+c[1];
-var isEscapePossible = function(blocked, source, target) {
-    const map = blocked.reduce((a, c) => a.set(hash(c), 1), new Map());
+const hash1 = c => c[0]*1000000+c[1];
+var isEscapePossible_numeric_key = function(blocked, source, target) {
+    const map = blocked.reduce((a, c) => a.set(hash1(c), 1), new Map());
     if (!blocked.length || (source[0] === target[0] && source[1] === target[1])) return true;
-    if (map.get(hash(target)) || map.get(hash(source))) return false;
+    if (map.get(hash1(target)) || map.get(hash1(source))) return false;
     const dir = [[1,0],[-1,0],[0,1],[0,-1]];
-    let maxX = 0, maxY = 0, minX = 1000000, minY = 1000000;
-    for (let [x, y] of blocked) {
-        maxX = Math.max(maxX, x);
-        maxY = Math.max(maxY, y);
-        minX = Math.min(minX, x);
-        minY = Math.min(minY, y);
-    }
-    const inside = ([x, y]) => minX < x && x < maxX && minY < y && y < maxY;
-    const dfs = ([sx, sy], [tx, ty]) => {
-        map.set(hash([sx, sy]), 1);
+    
+    const dfs = ([cx, cy], [sx, sy], [tx, ty], map) => { //current position, original position, target position
+        map.set(hash1([cx, cy]), 1);
         for (let [dx, dy] of dir) {
-            const [nx, ny] = [sx + dx, sy + dy];
-            if (nx === tx && ny === ty) return true;
-            if (nx ===)
+            const [nx, ny] = [cx + dx, cy + dy];
+            if (nx >= 0 && nx < 1000000 && ny >= 0 && ny < 1000000 && !map.has(hash1([nx, ny]))) { //check valid position
+                if ( nx === tx && ny === ty) return true; //if they meet in wall
+                if (Math.abs(nx - sx) + Math.abs(ny - sy) > 200) return true; //if it escape out of wall
+                map.set(hash1([nx, ny]), 1);
+                if (dfs([nx, ny], [sx, sy], [tx, ty], map)) return true; //if one of succeed, return true
+            }
         }
+        return false; //all fail, return false
     };
-    if (inside(source) && inside(target)) {
-        return dfs(source, target);
-    } else if (inside(target)) {
-        return dfs(target, source);
-    } else if (inside(source)){
-        return true;
-    }
+    return dfs(source, source, target, new Map(map)) && dfs(target, target, source, new Map(map)); //visited and block map need fresh copy.
+};
+
+const hash = c => `${c[0]}_${c[1]}`;  //timeout in large scale test
+var isEscapePossible_string_key = function(blocked, source, target) {
+    const map = blocked.reduce((a, c) => {a[hash(c)] = 1; return a}, {});
+    if (!blocked.length || (source[0] === target[0] && source[1] === target[1])) return true;
+    if (map[hash(target)] || map[hash(source)]) return false;
+    const dir = [[1,0],[-1,0],[0,1],[0,-1]];
+    
+    const dfs = ([cx, cy], [sx, sy], [tx, ty], map) => { //current position, original position, target position
+        map[hash([cx, cy])] = 1;
+        for (let [dx, dy] of dir) {
+            const [nx, ny] = [cx + dx, cy + dy];
+            if (nx >= 0 && nx < 1000000 && ny >= 0 && ny < 1000000 && !map[hash([nx, ny])]) { //check valid position
+                if ( nx === tx && ny === ty) return true; //if they meet in wall
+                if (Math.abs(nx - sx) + Math.abs(ny - sy) > 200) return true; //if it escape out of wall
+                map[hash([nx, ny])] = 1;
+                if (dfs([nx, ny], [sx, sy], [tx, ty], map)) return true; //if one of succeed, return true
+            }
+        }
+        return false; //all fail, return false
+    };
+    return dfs(source, source, target, {...map}) && dfs(target, target, source, {...map}); //visited and block map need fresh copy.
 };
 
 
-console.log(isEscapePossible([[0,1],[1,0]], source = [0,0], target = [0,2])); //false
-console.log(isEscapePossible([], source = [0,0], target = [999999,999999]));//true
-console.log(isEscapePossible([[691938,300406],[710196,624190],[858790,609485],[268029,225806],[200010,188664],[132599,612099],[329444,633495],[196657,757958],[628509,883388]]
+console.log(isEscapePossible_numeric_key([[0,1],[1,0]], source = [0,0], target = [0,2])); //false
+console.log(isEscapePossible_numeric_key([], source = [0,0], target = [999999,999999]));//true
+console.log(isEscapePossible_numeric_key([[691938,300406],[710196,624190],[858790,609485],[268029,225806],[200010,188664],[132599,612099],[329444,633495],[196657,757958],[628509,883388]]
     ,[655988,180910]
     ,[267728,840949])); //true
+
+console.log(isEscapePossible_numeric_key([[0,3],[1,0],[1,1],[1,2],[1,3]], [0,0], [0,2])); //true
