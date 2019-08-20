6012861096de0ef347cfed867249c9651d04d5ad
meng <xumeng@gmail.com>
2019-08-20 01:06:56 -0400

    Update $__implementAsyncAwait.js

--- $__implementAsyncAwait.js
+++ $__implementAsyncAwait.js
@@ -3,10 +3,26 @@ const api1 = new Promise((resolve, reject) => {
     setTimeout(() => { resolve(data); }, 1000);
 });
 
+async function do() {
+
+    const res1 = await api1();
+    console.log(`Response: ${res1}`);
+    const res2 = await api2(res1);
+    console.log(`Response: ${res2}`);
+    const res3 = await api2(res2);
+    console.log(`Response: ${res3}`);
+
+} 
+
+//implements by generator
 function* generator() {
     try {  //try catch could not catch async error, but promise then() err to make the gen throw erro and catch here.
-        const response = yield api1; //async call api1
-        console.log(`Response: ${response}`); //await to get response. Without yield, you will get undefined. 
+        const res1 = yield api1; //async call api1
+        console.log(`Response: ${res1}`); //await to get response. Without yield, you will get undefined. 
+        const res2 = yield api2(res1);
+        console.log(`Response: ${res2}`);
+        const res3 = yield api2(res2);
+        console.log(`Response: ${res3}`);
     } catch (err) {
         console.log(`Err: ${err}`);
     }
