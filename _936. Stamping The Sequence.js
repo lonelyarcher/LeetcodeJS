dcf6be6aae0796b7e37cac1b1b4ea72a43413a0a
mexu@doitt.nyc.gov <MeXu@doitt.nyc.gov>
2019-11-01 16:16:02 -0400

    wip

--- _936. Stamping The Sequence.js
+++ _936. Stamping The Sequence.js
@@ -2,15 +2,20 @@
 
 At the beginning, your sequence is target.length '?' marks.  You also have a stamp of lowercase letters.
 
-On each turn, you may place the stamp over the sequence, and replace every letter in the sequence with the corresponding letter from the stamp.  You can make up to 10 * target.length turns.
+On each turn, you may place the stamp over the sequence, and replace every letter in the sequence with the corresponding letter from the stamp.  
+You can make up to 10 * target.length turns.
 
-For example, if the initial sequence is "?????", and your stamp is "abc",  then you may make "abc??", "?abc?", "??abc" in the first turn.  (Note that the stamp must be fully contained in the boundaries of the sequence in order to stamp.)
+For example, if the initial sequence is "?????", and your stamp is "abc",  then you may make "abc??", "?abc?", "??abc" in the first turn.  
+(Note that the stamp must be fully contained in the boundaries of the sequence in order to stamp.)
 
-If the sequence is possible to stamp, then return an array of the index of the left-most letter being stamped at each turn.  If the sequence is not possible to stamp, return an empty array.
+If the sequence is possible to stamp, then return an array of the index of the left-most letter being stamped at each turn.  
+If the sequence is not possible to stamp, return an empty array.
 
-For example, if the sequence is "ababc", and the stamp is "abc", then we could return the answer [0, 2], corresponding to the moves "?????" -> "abc??" -> "ababc".
+For example, if the sequence is "ababc", and the stamp is "abc", then we could return the answer [0, 2], 
+corresponding to the moves "?????" -> "abc??" -> "ababc".
 
-Also, if the sequence is possible to stamp, it is guaranteed it is possible to stamp within 10 * target.length moves.  Any answers specifying more than this number of moves will not be accepted.
+Also, if the sequence is possible to stamp, it is guaranteed it is possible to stamp within 10 * target.length moves.  
+Any answers specifying more than this number of moves will not be accepted.
 
  
 
@@ -35,6 +40,20 @@ stamp and target only contain lowercase letters. */
  * @param {string} target
  * @return {number[]}
  */
+//Reverse the Simulation, the last stamp can be a????ca, then search backward to all ??????
 var movesToStamp = function(stamp, target) {
-    
+    const match = s => {
+        const ans = [];
+        for (let i = 0; i < s.length - 4; i++) {
+            let match = true;
+            for (let j = 0; j < 4; j++) {
+                if (target.charAt(i) !== s.charAt(i + j) && s.charAt(i + j) !== '?') {
+                    match = false;
+                    break;
+                }
+                if (match) ans.push(i);
+            }
+        }
+    }
+    return ans;
 };
