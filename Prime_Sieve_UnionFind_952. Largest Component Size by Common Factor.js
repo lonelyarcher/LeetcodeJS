3ace79e9a5974c67a4b066e369b71062835880fa
mexu@doitt.nyc.gov <MeXu@doitt.nyc.gov>
2019-11-01 09:34:48 -0400

    wip

--- Prime_Sieve_UnionFind_952. Largest Component Size by Common Factor.js
+++ Prime_Sieve_UnionFind_952. Largest Component Size by Common Factor.js
@@ -32,11 +32,10 @@ Note:
  */
 //Sieve to generate all the prime number within N, upper bound: O(nlog(log(n))) = n * (1/2 + 1/3 + 1/5 + 1/7 + ...) 
 const sieve = N => {
-    const size = N //~~Math.sqrt(N) is not right here. Prime can be larger than sqrt(N), like 926 -> 463, 463 is prime number.
-    const primes = Array(N).fill(true);
+    const primes = Array(N + 1).fill(true);
     primes[0] = primes[1] = false;
     let p = 2;
-    while (p <= N) {
+    while (p <= Math.sqrt(N)) {//the max of the smaller factor of two factors, N = f1 * f2, if f1 is smaller, f1 can not exceed sqrt
         if (primes[p]) {
             for (let i = 2 * p; i <= N; i += p) {
                 primes[i] = false;
@@ -48,9 +47,9 @@ const sieve = N => {
 }
 //Union Find to union to the prime factors
 var largestComponentSize = function(A) {
-    const primes = sieve(Math.max(...A));
+    const primes = sieve(~~(Math.max(...A)/2));//the largest prime factor can not be great than N/2
     const parent = [...Array(primes.length + A.length)].map((c, i) => i); //because we will union num to its prime factor to avoid collision, we add A.length to all the prime index.
-    const weight = Array(primes.length + A.length).fill(0);
+    const rank = Array(primes.length + A.length).fill(0);
     const find = v => {
         if (parent[v] !== v) {
             parent[v] = find(parent[v]);
@@ -60,11 +59,14 @@ var largestComponentSize = function(A) {
     const union = (v1, v2) => {
         const p1 = find(v1);
         const p2 = find(v2);
-        if (weight[p1] > weight[p2]) parent[p2] = p1;
-        else if (weight[p1] < weight[p2]) parent[p1] = p2;
-        else {
+        if (p1 === p2) return;
+        if (rank[p1] > rank[p2]) {
             parent[p2] = p1;
-            weight[p1]++;
+        } else if (rank[p1] < rank[p2]) {
+            parent[p1] = p2;
+        } else {
+            parent[p2] = p1;
+            rank[p1]++;
         }
     };
     for (let i = 0; i < A.length; i++) {
@@ -76,4 +78,5 @@ var largestComponentSize = function(A) {
     return Math.max(...Object.values(count));
 };
 
+console.log(largestComponentSize([1,2,3,4,5,6,7,8,9,10,11,12,13,14]));//11
 console.log(largestComponentSize([2,7,522,526,535,26,944,35,519,45,48,567,266,68,74,591,81,86,602,93,610,621,111,114,629,641,131,651,142,659,669,161,674,163,180,187,190,194,195,206,207,218,737,229,240,757,770,260,778,270,272,785,274,290,291,292,296,810,816,314,829,833,841,349,880,369,147,897,387,390,905,405,406,407,414,416,417,425,938,429,432,926,959,960,449,963,966,929,457,463,981,985,79,487,1000,494,508]));//84
